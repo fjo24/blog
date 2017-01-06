e81f51e18bd4349ffe2bb79409c0e6eb0aedb9ed
fjo24 <fjo224@gmail.com>
2017-01-06 11:38:58 -0430

    crud de listar usuarios

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,10 +1,8 @@
 @extends('admin.templates.main')
-
 @section('title', 'Lista de usuarios')
-
 @section('content')
 
-
+<a href="{{ route('admin.users.create')}}" class="btn btn-info">Registrar nuevo usuario</a>
 <table class="table table-striped">
   <thead>
   	<th>ID</th>
@@ -20,13 +18,19 @@
   		<td>{{ $user->id }}</td>	
   		<td>{{ $user->name }}</td>
   		<td>{{ $user->email }}</td>
-  		<td>{{ $user->type }}</td>
-  		<td><a href="" class="btn btn-danger"></a><a href="" class="btn btn-warning"></a></td>
+  		<td>
+  		@if($user->type == "admin")
+  		<span class="label label-danger">{{ $user->type }}</span>
+  		@else
+  		<span class="label label-primary">{{ $user->type }}</span>
+  		@endif
+  		</td>
+  		<td><a href="" class="btn btn-danger glyphicon glyphicon-remove"></a><a href="" class="btn btn-warning glyphicon glyphicon-pencil"></a></td>
   	</tr>
   	@endforeach
   </tbody>
 </table>
 
-
+{!! $users->render() !!}
 
 @endsection
